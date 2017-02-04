1a0134d9495130f17e77bd93ad39e79486e7d375
John Swanson <john.d.swanson@gmail.com>
2017-02-03 17:40:03 -0800

    clean js

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject tictag "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :clean-targets ^{:protect false} ["resources/public/js" :target]
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :figwheel {:css-dirs ["resources/public/css"]}
