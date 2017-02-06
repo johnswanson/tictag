6c8ab347b9b98441fd14d6f41346a1823ca2c922
John Swanson <john.d.swanson@gmail.com>
2017-02-05 17:23:36 -0800

    improve matrix plot

--- src/cljs/tictag/subs.cljs
+++ src/cljs/tictag/subs.cljs
@@ -1,38 +1,130 @@
 (ns tictag.subs
   (:require [re-frame.core :refer [reg-sub subscribe]]
-            [cljs-time.format :as f]))
+            [cljs-time.format :as f]
+            [tictag.dates :refer [seconds-since-midnight days-since-epoch]]))
 
 (def formatter (f/formatters :date-time))
-
 (defn parse-date [ping]
   (assoc ping
          :local-time (f/parse formatter (:local-time ping))
          :str-local-time (:local-time ping)))
 
-(reg-sub :raw-pings (fn [db _] (:pings db [])))
-
-(reg-sub :parsed-pings
-         (fn [_ _] (subscribe [:raw-pings]))
-         (fn [raw-pings _]
-           (map parse-date raw-pings)))
-
 (reg-sub :ping-query (fn [db _] (:ping-query db)))
 
-(reg-sub :query-fn
-         (fn [_ _] (subscribe [:ping-query]))
-         (fn [ping-query]
-           (if ping-query
-             (fn [{:keys [tags]}]
-               (tags (keyword ping-query)))
-             (constantly false))))
+(reg-sub
+ :query-fn
+ (fn [_ _] (subscribe [:ping-query]))
+ (fn [ping-query]
+   (if ping-query
+     (fn [{:keys [tags]}]
+       (tags (keyword ping-query)))
+     (constantly false))))
 
 (reg-sub
  :pings
- (fn [_ _]
-   [(subscribe [:parsed-pings])
-    (subscribe [:query-fn])])
- (fn [[parsed-pings query] _]
-   (map #(assoc % :active? (query %)) parsed-pings)))
+ (fn [db _]
+   (:pings db [])))
 
-(reg-sub :db first)
+(reg-sub
+ :ping-active?
+ (fn [_ _]
+   (subscribe [:query-fn]))
+ (fn [query-fn [_ ping]]
+   (query-fn ping)))
+
+(reg-sub
+ :parsed-times
+ (fn [_ _]
+   (subscribe [:pings]))
+ (fn [pings _]
+   (map #(f/parse formatter (:local-time %)) pings)))
+
+(reg-sub
+ :days-since-epoch
+ (fn [_ _]
+   (subscribe [:parsed-times]))
+ (fn [parsed-times _]
+   (map days-since-epoch parsed-times)))
+
+(reg-sub
+ :min-days-since-epoch
+ (fn [_ _]
+   (subscribe [:days-since-epoch]))
+ (fn [days _]
+   (apply min days)))
+
+(reg-sub
+ :max-days-since-epoch
+ (fn [_ _] (subscribe [:days-since-epoch]))
+ (fn [days _]
+   (apply max days)))
+
+(reg-sub
+ :seconds-since-midnight
+ (fn [_ _] (subscribe [:pings]))
+ (fn [pings _]
+   (map (comp seconds-since-midnight :local-time) pings)))
+
+(reg-sub
+ :matrix-plot-height
+ (constantly 500))
+(reg-sub
+ :matrix-plot-width
+ (constantly 500))
+
+(reg-sub
+ :matrix-plot-domain-x
+ (fn [_ _]
+   [(subscribe [:min-days-since-epoch])
+    (subscribe [:max-days-since-epoch])])
+ (fn [[min max] _]
+   [min max]))
+
+(reg-sub
+ :matrix-plot-domain-y
+ (constantly [0 (* 24 60 60)]))
+
+(reg-sub
+ :matrix-plot-range-x
+ (fn [_ _] (subscribe [:matrix-plot-width]))
+ (fn [width _] [0 (- width 10)]))
+
+(reg-sub
+ :matrix-plot-range-y
+ (fn [_ _] (subscribe [:matrix-plot-height]))
+ (fn [height _] [50 (- height 10)]))
+
+(defn pixel [domain range x]
+  (let [max-domain  (apply max domain)
+        min-domain  (apply min domain)
+        domain-diff (- max-domain min-domain)
+
+        max-range  (apply max range)
+        min-range  (apply min range)
+        range-diff (- max-range min-range)]
+    (+ min-range (* range-diff (/ (- x min-domain) domain-diff)))))
+
+(reg-sub
+ :ping-pixel
+ (fn [_ _]
+   [(subscribe [:matrix-plot-range-x])
+    (subscribe [:matrix-plot-range-y])
+    (subscribe [:matrix-plot-domain-x])
+    (subscribe [:matrix-plot-domain-y])])
+ (fn [[range-x range-y
+       domain-x domain-y] [_ {:keys [x y]}]]
+   {:x (pixel domain-x range-x x)
+    :y (pixel domain-y range-y y)}))
+
+(reg-sub
+ :matrix-plot-pings
+ (fn [_ _]
+   [(subscribe [:pings])
+    (subscribe [:seconds-since-midnight])
+    (subscribe [:days-since-epoch])])
+ (fn [[pings seconds days] _]
+   (map (fn [d s p]
+          (assoc p :x d :y s))
+        days seconds pings)))
+
 
--- src/cljs/tictag/views.cljs
+++ src/cljs/tictag/views.cljs
@@ -9,60 +9,30 @@
             [cljs-time.format :as f])
   (:import [goog.date.Interval]))
 
-(defn value-pixel-mapper [domain range]
-  (let [max-domain  (apply max domain)
-        min-domain  (apply min domain)
-        domain-diff (- max-domain min-domain)
+(defn datapoint [ping]
+  (let [active? (subscribe [:ping-active? ping])
+        pixel   (subscribe [:ping-pixel ping])]
+    (fn [ping]
+      [:circle {:on-mouse-over #(js/console.log ping)
+                :cx (:x @pixel)
+                :cy (:y @pixel)
+                :r 3
+                :style {:opacity (if @active? "0.8" "0.2")}
+                :fill  (if @active? "purple" "black")}])))
 
-        max-range  (apply max range)
-        min-range  (apply min range)
-        range-diff (- max-range min-range)]
-    {:pixel-fn (fn [x]
-                 (+ min-range (* range-diff (/ (- x min-domain) domain-diff))))
-     :value-fn (fn [p]
-                 (+ min-domain (* domain-diff (/ p range-diff))))}))
-
-(defn datapoint [{:keys [xpixel ypixel str-local-time local-time] :as d} active?]
-  [:circle {:on-mouse-over #(js/console.log str-local-time local-time)
-            :cx    xpixel
-            :cy    ypixel
-            :r     3
-            :style {:opacity (if active? "0.8" "0.2")}
-            :fill  (if active? "purple" "black")}])
-
-
-(defn graph [width height pings]
-  [:div
-   (when (seq pings)
-     (let [data              (map #(let [local-time (:local-time %)
-                                         str-local-time (:str-local-time %)]
-                                     (assoc %
-                                            :x (days-since-epoch local-time)
-                                            :y (seconds-since-midnight str-local-time)))
-                                  pings)
-           xs                (map :x data)
-           ys                (map :y data)
-           x-range           [0 (- width 10)]
-           y-range           [50 (- height 10)]
-           x-domain          [(apply min xs) (apply max xs)]
-           y-domain          [0 (* 24 60 60)]
-           {x-fn  :pixel-fn
-            x-inv :value-fn} (value-pixel-mapper x-domain x-range)
-
-           {y-fn  :pixel-fn
-            y-inv :value-fn} (value-pixel-mapper y-domain y-range)
-           data              (map #(assoc %
-                                          :xpixel (x-fn (:x %))
-                                          :ypixel (y-fn (:y %)))
-                                  data)]
-       [:svg {:style {:width (str width "px") :height (str height "px")}}
-        [:g {:style {:stroke "black" :stroke-width 1}}
-         [:line {:x1 0 :x2 0 :y1 0 :y2 height}]
-         [:line {:x1 0 :x2 width :y1 height :y2 height}]]
-        [:g
-         (for [d data]
-           ^{:key (str (:local-time d))}
-           [datapoint d (:active? d)])]]))])
+(defn matrix-plot []
+  (let [width  (subscribe [:matrix-plot-width])
+        height (subscribe [:matrix-plot-height])
+        pings  (subscribe [:matrix-plot-pings])]
+    (fn []
+      [:svg {:style {:width (str @width "px") :height (str @height "px")}}
+       [:g {:style {:stroke "black" :stroke-width 1}}
+        [:line {:x1 0 :x2 0 :y1 0 :y2 @height}]
+        [:line {:x1 0 :x2 @width :y1 @height :y2 @height}]]
+       [:g
+        (for [ping @pings]
+          ^{:key (:local-time ping)}
+          [datapoint ping])]])))
 
 (defn app
   []
@@ -72,5 +42,6 @@
        [:span {:on-click #(dispatch [:fetch-pings])} "Click Me"]
        [:input {:type :text
                 :on-change #(dispatch [:update-ping-query (.. % -target -value)])}]
-       [graph 500 500 @pings]])))
+       [matrix-plot]])))
+
 
