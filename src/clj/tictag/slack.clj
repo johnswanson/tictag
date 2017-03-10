2204c097c35779512e3632a74a6737173f92a48d
John Swanson <john.d.swanson@gmail.com>
2017-03-09 17:34:14 -0800

    for now, don't use rtm slack API

--- src/clj/tictag/slack.clj
+++ src/clj/tictag/slack.clj
@@ -1,24 +1,27 @@
 (ns tictag.slack
-  (:require [slack-rtm.core :as slack]
-            [org.httpkit.client :as http]
+  (:require [org.httpkit.client :as http]
             [com.stuartsierra.component :as component]
             [cheshire.core :as json]
             [tictag.cli :refer [handle-command]]
             [tictag.db :as db]
             [clojure.string :as str]))
 
-(defn dispatcher [slack]
-  (:dispatcher (:conn slack)))
+(def slack-api-url "https://slack.com/api/")
+(defn method-url [m] (str slack-api-url m))
+(defn slack-call! [cmd params]
+  (-> (method-url cmd)
+      (http/post {:form-params params})
+      deref
+      :body
+      (json/parse-string true)))
 
-(defn pub [slack]
-  (:events-publication (:conn slack)))
 
 (defn send-message! [slack body]
-  (let [{:keys [dm-channel-id]} slack]
-    (slack/send-event (dispatcher slack)
-                      {:type "message"
-                       :text body
-                       :channel dm-channel-id})))
+  (let [{:keys [dm-channel-id config]} slack]
+    (slack-call! "chat.postMessage"
+                 {:token   (:token config)
+                  :channel dm-channel-id
+                  :text    body})))
 
 (defn handle-message [component message]
   (when (and (= (:dm-channel-id component) (:channel message))
@@ -37,29 +40,14 @@
 (defrecord Slack [config db]
   component/Lifecycle
   (start [component]
-    (let [user-list     (-> "https://slack.com/api/users.list"
-                            (http/get {:query-params {:token (:token config)}})
-                            deref
-                            :body
-                            (json/parse-string true)
-                            :members)
+    (let [user-list     (:members (slack-call! "users.list" {:token (:token config)}))
           [my-user]     (filter #(= (:name %) (:username config)) user-list)
-          dm-channel-id (-> "https://slack.com/api/im.open"
-                            (http/get {:query-params {:token (:token config)
-                                                      :user  (:id my-user)}})
-                            deref
-                            :body
-                            (json/parse-string true)
-                            :channel
-                            :id)
-          conn          (slack/connect (:token config))
-          new-component (assoc component
-                               :user my-user
-                               :conn conn
-                               :dm-channel-id dm-channel-id)]
-      (slack/sub-to-event (:events-publication conn) :message (partial handle-message new-component))
-      new-component))
-  (stop [component]
-    (when-let [conn (:conn component)]
-      (slack/send-event (:dispatcher conn) :close))))
+          dm-channel-id (:id
+                         (:channel
+                          (slack-call!
+                           "im.open"
+                           {:token (:token config)
+                            :user  (:id my-user)})))]
+      (assoc component :user my-user :dm-channel-id dm-channel-id)))
+  (stop [component] component))
 
