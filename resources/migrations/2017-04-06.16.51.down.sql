73c028f602a07badd69ccd3d7b9ffa71c8a1d6c8
John Swanson <john.d.swanson@gmail.com>
2017-04-14 15:37:30 -0700

    remove test user from migrations

--- resources/migrations/2017-04-06.16.51.down.sql
+++ resources/migrations/2017-04-06.16.51.down.sql
@@ -1,9 +1,10 @@
 DROP TABLE slack;
 DROP TABLE beeminder_goals;
 DROP TABLE beeminder;
-DELETE FROM pings WHERE user_id != 1;
+DELETE FROM pings;
 DROP INDEX "pings_user_id_idx";
 ALTER TABLE pings DROP CONSTRAINT pings_pkey;
 ALTER TABLE pings DROP COLUMN user_id;
 ALTER TABLE pings ADD PRIMARY KEY (ts);
+DROP TABLE pings_backup;
 DROP TABLE users;
--- resources/migrations/2017-04-06.16.51.up.sql
+++ resources/migrations/2017-04-06.16.51.up.sql
@@ -7,23 +7,9 @@ pass CHAR(98) NOT NULL,
 tz VARCHAR(1024) NOT NULL UNIQUE
 );
 
-INSERT INTO users
-(
-"username",
-"email",
-"pass",
-"tz"
-)
-VALUES
-(
-'j',
-'j@agh.io',
-'bcrypt+sha512$bf75bd7e329fdd9c9b50614ce9142e10$12$56e7c0e1b0193b370ddf64961f8cef1f3320310ed0a2d7cf',
-'America/Los_Angeles'
-);
-
+CREATE TABLE pings_backup AS SELECT * FROM pings;
+DELETE FROM pings;
 ALTER TABLE pings ADD user_id BIGINT REFERENCES users(id);
-UPDATE pings SET user_id=1;
 ALTER TABLE pings DROP CONSTRAINT pings_pkey;
 ALTER TABLE pings ADD PRIMARY KEY (user_id, ts);
 CREATE INDEX "pings_user_id_idx" ON pings (user_id);
@@ -57,18 +43,3 @@ goal VARCHAR(1024),
 tags TEXT
 );
 
-INSERT INTO beeminder
-(user_id, username, encrypted_token, encryption_iv, is_enabled)
-VALUES
-(1, 'tictagtest', '\xbf967e499a49501c7c691b79837782ea568cf57be3c3bc268a57443d483c40b13284f975417c588d5e6ebf6ad3dae7a0', '\x3488f93aa3b2513813d9646040e8825b', TRUE);
-INSERT INTO slack
-(user_id, slack_user_id, username, encrypted_bot_access_token, encryption_iv, channel_id)
-VALUES
-(1, 'U0L05K5D3', 'john', '\x2c58a29dc1a872f3d32d14352b3bf158ad39e6e1d60dcac4acde41dc49fe80c824940626801ee3e34bfeb9b89f1ccb3971ee86a4db22514c57928ed90da185b4', '\x58402c1258619023e3c00f995b8643b0', 'D4HJSHY2K');
-
-INSERT INTO beeminder_goals
-(beeminder_id, goal, tags)
-VALUES
-(1, 'test', ':coding'),
-(1, 'test2', '[:or :play :web]'),
-(1, 'test3', '[:and :play :web]');
