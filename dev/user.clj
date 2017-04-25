8a57d5f75e9417c20b40e21ee3167b962c6556ac
John Swanson <john.d.swanson@gmail.com>
2017-04-25 16:31:40 -0700

    function to add a development user

--- dev/user.clj
+++ dev/user.clj
@@ -11,6 +11,7 @@
             [clojure.test :refer :all]
             [tictag.server-test :as server-test]
             [tictag.ragtime :as ragtime]
+            [tictag.tagtime :as tt]
             [ragtime.repl :refer [migrate rollback]]
             [honeysql.core :as sql]
             [honeysql.helpers :refer :all]
@@ -30,4 +31,60 @@
 
 (reloaded.repl/set-init! #(tictag.system/system config))
 
-(defn dev-user [] (db/get-user (:db system) "j"))
+(defn dev-user [] (db/get-user (:db system) "TEST_USER"))
+
+(def my-user-id (-> (select :id)
+                    (from :users)
+                    (where [:= :username "TEST_USER"])
+                    (limit 1)))
+
+(def my-beeminder-id (-> (select :beeminder.id)
+                         (from :beeminder)
+                         (join :users [:= :users.id my-user-id])))
+
+(defn make-dev-user []
+  (j/with-db-transaction [tx (-> system :db :db)]
+    (j/execute! tx
+                (-> (insert-into :users)
+                    (values [{:username "TEST_USER"
+                              :email    "tictag-test@agh.io"
+                              :pass     (db/hashp "foobar")
+                              :tz       "America/Los_Angeles"}])
+                    (upsert (-> (on-conflict :username)
+                                (do-nothing)))
+                    sql/format))
+    (let [{:keys [encrypted iv]} (crypto/encrypt (env :dev-beeminder-token)
+                                                 (-> system :db :crypto-key))]
+      (j/execute! tx
+                  (-> (insert-into :beeminder)
+                      (values [{:user_id         my-user-id
+                                :username        "tictagtest"
+                                :encrypted_token encrypted
+                                :encryption_iv   iv
+                                :is_enabled      true}])
+                      (upsert (-> (on-conflict :user_id)
+                                  (do-update-set :encrypted_token :encryption_iv)))
+                      sql/format)))
+    (let [{:keys [encrypted iv]} (crypto/encrypt
+                                  (env :dev-slack-bot-token)
+                                  (-> system :db :crypto-key))]
+      (j/execute! tx
+                  (-> (insert-into :slack)
+                      (values [{:user_id                    my-user-id
+                                :username                   (env :dev-slack-username)
+                                :encrypted_bot_access_token encrypted
+                                :encryption_iv              iv
+                                :channel_id                 (env :dev-slack-channel-id)
+                                :slack_user_id              (env :dev-slack-user-id)}])
+                      (upsert (-> (on-conflict :user_id)
+                                  (do-update-set :encrypted_bot_access_token
+                                                 :encryption_iv)))
+                      sql/format)))
+    (j/execute! tx
+                (-> (insert-into :beeminder_goals)
+                    (values [{:beeminder_id my-beeminder-id
+                              :goal         "test"
+                              :tags         (pr-str [:and :this :that])}])
+                    (upsert (-> (on-conflict :beeminder_id :goal)
+                                (do-nothing)))
+                    sql/format))))
