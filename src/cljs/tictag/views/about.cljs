1678bc7f79005050501d328a051f7bfac9633fc6
John Swanson <john.d.swanson@gmail.com>
2017-08-25 16:41:17 -0700

    add more graphs to about page

--- src/cljs/tictag/views/about.cljs
+++ src/cljs/tictag/views/about.cljs
@@ -25,19 +25,37 @@
                     [re-com/p "This random sampling of your time provides you with a statistically accurate picture of where your time "
                      [:i "actually goes"] "."]
                     [re-com/p
-                     "So how much time do I spend coding? TicTag knows!"]
+                     "So how much time do I spend coding? TicTag knows! (Click on the graph to see a larger version)"]
                     (let [coding-link "https://agh.io/api/graph?sig=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJxdWVyeSI6IihvciBkZXYgY29kaW5nKSIsInVzZXItaWQiOjF9.MGUCMQDGJJPTKcZVZAWGkAt5Nf_g5GdTg5O2jRVA4jCdIFbPAT5Soouq84vMh0E9_CGevrICMFqtrArVGuEvxNEWblhy2nxJzYOSLZUmNITTOVqPMHil8SKDNVnEHZHwZkTPLA90ow"]
-                      [:a {:href coding-link
+                      [:a {:href   coding-link
                            :target :_blank}
                        [:img {:width "850px"
-                              :src coding-link}]])
+                              :title "Time spent coding"
+                              :src   coding-link}]])
                     [re-com/p
                      "How much time do I spend working on this project? TicTag knows!"]
                     (let [ttc-link "https://agh.io/api/graph?sig=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJxdWVyeSI6InR0YyIsInVzZXItaWQiOjF9.MGQCMG9rkoU6_pBojnXLiHnl6Lr5wna2DZbMWI29Sw65SzCV95irHW7Jb4fu7jSNbo1utgIwS886Iyr_y9JGU1dG3seQ0J-6uZl0NNXgEin08YfeORLoVHAgUhsWgUevG6xwY4Fe"]
-                      [:a {:href ttc-link
+                      [:a {:href   ttc-link
                            :target :_blank}
                        [:img {:width "850px"
-                              :src ttc-link}]])
+                              :title "Time spent working on TTC"
+                              :src   ttc-link}]])
+                    [re-com/p
+                     "How about reading?"]
+                    (let [read-link "https://agh.io/api/graph?sig=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJxdWVyeSI6InJlYWQiLCJ1c2VyLWlkIjoxfQ.MGUCMHd1XcbvhSfx0jysiNVK73DjBHZts3IcqB5RVqd9MhaZIgtiJwsV4YXoXhLkGVPVywIxAKVL9eqyKuYIl3MQR2etSNqpd2-hG7u-Hh0fHyt8gQWrD_g2vtM-N_XQl5x03ftiRg"]
+                      [:a {:href   read-link
+                           :target :_blank}
+                       [:img {:width "850px"
+                              :title "Time spent reading"
+                              :src   read-link}]])
+                    [re-com/p
+                     "Cooking or cleaning?"]
+                    (let [cook-link "https://agh.io/api/graph?sig=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJxdWVyeSI6IihvciBjb29rIGNsZWFuKSIsInVzZXItaWQiOjF9.MGQCMFDCcyZyQ2XUQoEFarSU_r-w-zBuRGmWxZs7Lae8ZkPCR4y7CWoEK-THB78LHiuwRAIwOsymQQGchg6wuSMGsonPwQHWAq8yN7VJ9gSagRfacc8k_HHGjenR1PuwkhsXjx_k"]
+                      [:a {:href   cook-link
+                           :target :_blank}
+                       [:img {:width "850px"
+                              :title "Time spent cooking or cleaning"
+                              :src   cook-link}]])
                     [re-com/p "TicTag also knows that on an average day, I spend:"]
                     [:ul
                      [:li "7h30m sleeping"]
